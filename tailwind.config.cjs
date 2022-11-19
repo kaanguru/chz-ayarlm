c90ab819655aca695a04ee6eee95e076dba56833
kaanguru <kaanguru@mail.ru>
2022-11-19 21:53:26 +0300

    style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,23 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+ 
+					"primary": "#a77a5b",
+					"secondary": "#ba8d66",
+					"accent": "#fb8b24",
+					"neutral": "#ec9192",
+					"base-100": "#ededf4",
+					"info": "#0f4c5c",
+					"success": "#9ea93f",
+					"warning": "#f2af29",
+					"error": "#F87272",
+				},
+			},
+		],
+	},
 	plugins: [require("daisyui")],
 }
